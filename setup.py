585f5d6b311f49d59d322f7e204545490a8a4620
aganezov <aganezov@gwu.edu>
2015-11-26 13:37:27 -0500

    added setuptools support for setup.py script added support for `setup.py test` invocation

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(name='gos-asm',
-      version='0.0',
+      version='0.0.0-a',
       description='Multi-genome gene order based assembler',
       install_requires=list(map(lambda entry: entry.strip(), open("requirements.txt", "rt").readlines())),
       author='Sergey Aganezov',
@@ -12,6 +15,7 @@ setup(name='gos-asm',
       keywords=["breakpoint graph", "data structures", "python", "scaffolding", "gene order", "assembly", "genome"],
       url='https://github.com/aganezov/gos-asm',
       packages=['gos_asm', 'tests'],
+      test_suite="tests",
       classifiers=[
           "Development Status :: 1 - Planning",
           "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
